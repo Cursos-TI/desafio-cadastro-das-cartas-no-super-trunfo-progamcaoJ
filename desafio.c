9523a68b4eaf577a27ff62b2d72305a45490cbb1
progamcaoJ <joaofeijosilva@hotmail.com>
2025-02-20 19:58:29 +0000

    modified:   desafio

--- desafio.c
+++ desafio.c
@@ -3,6 +3,17 @@
 int main (){
        printf ("Desafio Super Trunfo !\n");
 
+       // Cadastro Carta 1 
+      char estado = "RJ";
+      char codigo = "R01";
+      char nome = 'Rio de Janeiro';
+      int população = '2,611';
+      int pontos = '58';
+      float pib = '271,30';
+      float área = '1.200';
+
+      
+
        
 
 }
