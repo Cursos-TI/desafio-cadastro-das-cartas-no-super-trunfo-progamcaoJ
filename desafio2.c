935e25c0413194e1d850b9a9e9817922f8c4dc7b
progamcaoJ <joaofeijosilva@hotmail.com>
2025-02-20 20:17:51 +0000

    new file:   desafio2

---
+++ desafio2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Desafio Super Trunfo Paises !\n");
+  int brasil1, japao2;
+  brasil1 = 216.40;
+  japao2 = 124.50;
+
+  if (brasil1 > japao2)
+  {
+    printf("Brasil é maior do que Japão\n");
+  }
+  else
+  {
+    printf("Em números de população o Brasil é maior");
+  }
+
+  printf("O país vencedor é:  &Brasil");
+}
